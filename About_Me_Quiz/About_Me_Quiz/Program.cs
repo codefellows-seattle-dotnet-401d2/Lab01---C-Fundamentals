b039ecc0e22866f9798060e179eb444fbabb4952
QueensKid <andres.ibarra0716@gmail.com>
2017-12-19 11:06:21 -0800

    hope it works

---
+++ About_Me_Quiz/About_Me_Quiz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name stranger?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Welcome {userName}, Answer some questions to reveal my true intentions");
+            Console.ReadLine();
+        }
+
+        static void MyQuestions()
+        {
+            string[] questions = new string[5];
+            questions[0] = "What is my favorite color?";
+            questions[1] = "How many countries do you think I have visited?";
+            questions[2] = "Soccer or Football?";
+            questions[3] = "what is the capitol of ";
+            questions[4] = "";
+
+        }
+
+    }
+}
