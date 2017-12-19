42953e3fba457345f082fbd0202dca7e220db2d8
Matt <wrestler.411@comcast.net>
2017-12-19 10:38:20 -0800

    Initial Commit

---
+++ quiz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AboutMe
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Thanks for visiting my About Me page!");
+            Console.WriteLine("I'm Matt, and you'll be answering questions about me!");
+            string username = Console.ReadLine();
+            Console.WriteLine($"Welcome {username}, time to get into the questions!")
+        }
+    }
+}
